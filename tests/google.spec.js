09c071c604e9a42f95825abe6f9440f9bb6b02d3
Adil Esengarin <a.esengarin@a1qa.com>
2025-11-26 16:47:25 +0500

    commit

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -1,16 +1,15 @@
 import { test, expect } from "@playwright/test";
-import { step } from "allure-js-commons";
+import { step, attachment } from "allure-js-commons";
 
 test("Google title should contain Google", async ({ page }) => {
-  await step("Открываем главную страницу", async () => {
-    await page.goto("https://google.com");
+  await step("Navigate Hello world", async () => {
+  await page.goto("https://google.com");
   });
+  await attachment("Screenshot", await page.screenshot(), "image/png")
   await expect(page).toHaveTitle(/Google/);
 });
 
 test("This test will fail intentionally", async ({ page }) => {
-  await step("Open Google homepage", async () => {
-    await page.goto("https://google.com");
-  });
+  await page.goto("https://google.com");
   await expect(page).toHaveTitle(/SomethingElse/);
 });
